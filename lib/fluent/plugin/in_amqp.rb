dde3796cf566928e29cc6892545f6f7a23f706db
Valeri Sokolov <ulfurinn@ulfurinn.net>
2014-06-23 22:43:25 +0200

    using TextParser to handle the payload

--- lib/fluent/plugin/in_amqp.rb
+++ lib/fluent/plugin/in_amqp.rb
@@ -17,17 +17,23 @@ module Fluent
     config_param :exclusive, :bool, :default => false
     config_param :auto_delete, :bool, :default => false
     config_param :passive, :bool, :default => false
-    config_param :payload_format, :string, :default => "json"
+    config_param :payload_format, :string, :default => "json" # legacy
 
     def initialize
       require 'bunny'
-      require "json" if @payload_format == "json"
-
       super
     end
 
     def configure(conf)
+      conf['format'] ||= conf['payload_format'] # legacy
+
       super
+
+      parser = TextParser.new
+      if parser.configure(conf, false)
+        @parser = parser
+      end
+
       @conf = conf
       unless @host && @queue
         raise ConfigError, "'host' and 'queue' must be all specified."
@@ -60,19 +66,20 @@ module Fluent
 
     private
     def parse_payload(msg)
-      ret = { 'payload' => msg }
-
-      begin
-        case @payload_format
-        when "json"
-          ret = JSON.parse(msg)
+      if @parser
+        parsed = nil
+        @parser.parse msg do |_, payload|
+          if payload.nil?
+            log.warn "failed to parse #{msg}"
+            parsed = { "payload" => msg }
+          else
+            parsed = payload
+          end
         end
-      rescue => e
-        # should raises a error to ovserver?
-        $log.error "parse payload error: #{e}, payload: #{msg}"
+        parsed
+      else
+        { "payload" => msg }
       end
-
-      ret
     end
   end # class AMQPInput
 
